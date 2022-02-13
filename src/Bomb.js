eddd204b0defdce65f88df742074dcbe65dd7035
Nils van der Werf <n.vanderw.92@gmail.com>
2022-02-13 19:21:55 +1100

    Created a ticking time bomb

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,37 @@
-// your Bomb code here!
+import React, { Component } from 'react'
+
+class Bomb extends Component {
+  constructor(props) {
+      super(props)
+      this.state = {
+        secondsLeft: props.initialCount
+      }
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+        this.setState({
+          secondsLeft: this.state.secondsLeft - 1,
+        });
+      }, 1000);
+  }
+
+  componentWillUnmount() {
+    if (this.state.secondsLeft === 0) {
+        clearInterval(this.interval);
+    }
+  }
+
+  render() {
+    if (this.state.secondsLeft > 0) {
+    return (
+        <p>{`${this.state.secondsLeft} seconds left before I go boom!`}</p>
+    )
+    } 
+    else {
+        return <p>Boom!</p>
+    }
+  }
+}
+
+export default Bomb
